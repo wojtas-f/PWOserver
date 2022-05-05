37469447e920632b29fd621d493e840903e18bee
f.wojtas <wojtas_f@outlook.com>
2022-05-05 08:26:22 +0200

    refactor: move routes to separate files

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 
-const Swagger = require("./swagger");
+const Swagger = require("./routes/swagger");
+const appRouter = require("./routes/app");
 const userRouter = require("./routes/user");
 
 const app = express();
 const server = require("http").createServer(app);
 
-app.get("/", (req, res) => {
-  res.json({ msg: "hello world" });
-});
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cookieParser());
 
 app.use(Swagger);
 app.use(userRouter);
+app.use(appRouter);
 
 module.exports = server;
