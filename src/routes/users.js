60ecd02ed454c4865420327e6a8a439dcfba51e0
f.wojtas <wojtas_f@outlook.com>
2022-06-09 14:11:53 +0200

    feat: add validation for user iputs

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = new express.Router();
 const crypto = require("crypto");
 const db = require("../config/db.config");
-const userValidationRules = require("../validators/users");
+const {
+  validateCreateNewUser,
+  validateGetUser,
+} = require("../validators/users");
 const validate = require("../validators/validator");
 
 const getHashedPassword = (password) => {
@@ -18,14 +21,42 @@ const getHashedPassword = (password) => {
  *      get:
  *          tags:
  *              - users
- *          description: Get list of users
+ *          description: Get list of users or a single user
+ *          parameters:
+ *              - in: path
+ *                name: id
+ *                schema:
+ *                  type: integer
+ *                required: false
+ *                description: Unique user identifier
+ *              - in: path
+ *                name: email
+ *                schema:
+ *                  type: string
+ *                required: false
+ *                description: User email address
  *          responses:
  *              200:
  *                  description: List of users
  *              400:
  *                  description: Unable to get user list
  */
-router.get("/users", async (req, res) => {
+router.get("/users", validateGetUser(), validate, async (req, res) => {
+  const id = req.params.id;
+  const email = req.params.email;
+  console.log("user id:", id);
+  if (id) {
+    const { rows } = await db.query(`SELECT * FROM Users WHERE id = $1;`, [id]);
+    return res.json(rows);
+  }
+
+  if (email) {
+    const { rows } = await db.query(`SELECT * FROM Users WHERE email = $1;`, [
+      email,
+    ]);
+    return res.json(rows);
+  }
+
   const { rows } = await db.query(`SELECT * FROM Users;`);
   res.json(rows);
 });
@@ -67,48 +98,12 @@ router.get("/users", async (req, res) => {
  *              400:
  *                  description: Unable to create user
  */
-router.post("/users/add", userValidationRules(), validate, (req, res) => {
-  /**
-   * Return error if any field is invalid
-   */
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
+router.post("/users/add", validateCreateNewUser(), validate, (req, res) => {
   const body = req.body;
   body.password = getHashedPassword(body.password);
   console.log(body);
 
   return res.json(body);
-});
-
-/**
- * @swagger
- *
- * /users/get/{id}:
- *      get:
- *          tags:
- *              - users
- *          description: Get list of users
- *          parameters:
- *              - in: path
- *                name: id
- *                schema:
- *                  type: integer
- *                required: true
- *                description: Numeric ID of the user to get
- *          responses:
- *              200:
- *                  description: List of users
- *              400:
- *                  description: Unable to get user list
- */
-router.get("/users/get/:id", async (req, res) => {
-  const id = req.params.id;
-  console.log("user id:", id);
-  const { rows } = await db.query(`SELECT * FROM Users WHERE id = $1;`, [id]);
-  res.json(rows);
 });
 
 module.exports = router;
--- src/validators/users.js
+++ src/validators/users.js
@@ -1,10 +1,12 @@
 const { body } = require("express-validator");
 const db = require("../config/db.config");
 
-const userValidationRules = () => {
+const validateCreateNewUser = () => {
   return [
+    // Validate email address
     body("email")
       .isEmail()
+      .bail()
       .custom(async (emailString) => {
         const { rows } = await db.query(
           `SELECT * FROM Users WHERE email = $1;`,
@@ -14,16 +16,26 @@ const userValidationRules = () => {
           throw new Error("Email already in use");
         }
       }),
+    // Validate password
     body("password")
       .isLength({ min: 9 })
       .withMessage("Password must be at least 9 characters long"),
+    // Validate first name of a user
     body("firstName")
       .isLength({ min: 2 })
       .withMessage("First name must be at least 2 characters long"),
+    // Validate last name of a user
     body("lastName")
       .isLength({ min: 2 })
       .withMessage("Last name must be at least 2 characters long"),
   ];
 };
 
-module.exports = userValidationRules;
+const validateGetUser = () => {
+  return [
+    body("email").isEmail().optional().withMessage("Incorrect email address"),
+    body("id").isInt().optional().withMessage("Incorrect ID"),
+  ];
+};
+
+module.exports = { validateCreateNewUser, validateGetUser };
